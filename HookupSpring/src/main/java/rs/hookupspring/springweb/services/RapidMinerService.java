5dc3dca3013fc352ec3c713b5b8f493470c91c66
SayyedBandar <bandjur1993@gmail.com>
2017-02-04 17:23:30 +0100

    rapid miner integration into Hookup spring backend

---
+++ HookupSpring/src/main/java/rs/hookupspring/springweb/services/RapidMinerService.java
@@ -0,0 +1,17 @@
+package rs.hookupspring.springweb.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Bandjur on 1/7/2017.
+ */
+@Service
+public class RapidMinerService {
+
+    public void RunHookupProcess() {
+
+//        RapidMiner.setExecutionMode(RapidMiner.ExecutionMode.COMMAND_LINE);
+//        RapidMiner.init();
+    }
+
+}
